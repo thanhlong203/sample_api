94ee66d7723a2ebd47e1d914ca350ec2d930f6c6
long.dang.thanh <long.dang@bluebelt.asia>
2022-02-28 17:02:41 +0700

    first commit

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\Http\Response;
+use Cake\View\Exception\MissingTemplateException;
+use App\Controller\Component\PhpAesCipher;
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from templates/Pages/
+ *
+ * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
+ */
+class AccountController extends AppController
+{
+    /**
+     * Displays a view
+     *
+     * @param string ...$path Path segments.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
+     *   be found and in debug mode.
+     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
+     *   be found and not in debug mode.
+     * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
+     */
+    public function login()
+    {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $data = $this->request->getData();
+        if (!$data['displayName'] || !$data['password'] || $data['email']) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+        $response = $data;
+        $response['id'] = 1;
+        $response['walletAddress'] = "xkjhfgkhsdfjhgsdhfgsdf";
+        unset($response['password']);
+        echo json_encode($response);
+        exit();
+    }
+
+    public function info() {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $loginId = $this->request->getParam('loginId');
+        if (!$loginId) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+
+        $res = ['id' => 1, 'companyId' => 1, 'displayName' => 'awesome company', 'email' => 'test@gmail.com', 'lang' => 1, 'walletAddress'=>'xkjhfgkhsdfjhgsdhfgsdf'];
+        echo json_encode($res);
+        exit();
+    }
+
+    public function update() {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $data = $this->request->getData();
+        if (!$data['id']) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+        $response = $data;
+        $response['walletAddress'] = "xkjhfgkhsdfjhgsdhfgsdf";
+        $response['companyId'] = "1";
+        $response['email'] = "test@gmail.com";
+        $response['lang'] = "1";
+        unset($response['password']);
+        echo json_encode($response);
+        exit();
+    }
+
+    public function sso() {
+        $data = $this->request->getData();
+        $password = hash('sha256', $data['password']);
+        $now= round(microtime(true) * 1000);
+        $encryptObj = (object) ['password' => $password, 'email' => $data['email'], 'time' => $now];
+        $hash = PhpAesCipher::encrypt("@rtrans$2022", "artg3ne$2022", json_encode($encryptObj));
+        return $this->redirect("https://homepage-artnft-uat.bluebelt.asia?hash=".$hash.'&time='.$now);
+    }
+}
---
+++ src/Controller/ItemController.php
@@ -0,0 +1,234 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\Http\Response;
+use Cake\View\Exception\MissingTemplateException;
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from templates/Pages/
+ *
+ * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
+ */
+class ItemController extends AppController
+{
+    /**
+     * Displays a view
+     *
+     * @param string ...$path Path segments.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
+     *   be found and in debug mode.
+     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
+     *   be found and not in debug mode.
+     * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
+     */
+    public function create()
+    {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $data = $this->request->getData();
+        if (!$data['name'] || !$data['url'] || !$data['price']) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+        $response = $data;
+        $response['id'] = 1;
+        $response['walletAddress'] = "1";
+        $response['companyId'] = "1";
+        $response['productId'] = "1";
+        $response['variantId'] = "1";
+        // $response['title'] = "Hat";
+        // $response['name'] = "Nice Hat";
+        $response['quantity'] = "1";
+        // $response['price'] = "1000.00";
+        // $response['url'] = "hat.com";
+        $response['previewUrl'] = "abcde";
+        // $response['numberOfCopies'] = "1";
+        $response['serviceFee'] = "1";
+        $response['unlockContent'] = "1";
+        $response['lockContent'] = "1";
+        // $response['description'] = "no desc";
+        $response['storeId'] = "1";
+        $response['externalUrl'] = "external.com";
+        $response['itemType'] = "1";
+        // $response['referenceId'] = "refer.com";
+        // $response['collectionId'] = "1";
+        $response['referenceId'] = "1";
+        $response['itemRoute'] = "1";
+        $response['accountId'] = "1";
+        $response['creatorId'] = "1";
+        $response['originalItemId'] = "1";
+        $response['status'] = "1";
+        $response['liker'] = "1";
+        $response['expireTime'] = "2022-01-01";
+        $response['minimumBid'] = "1";
+        $response['highestBid'] = "1";
+        $response['listTime'] = "2022-01-01";
+        $response['tokenIds'] = "xasdfsdtwerwer";
+        $response['transactionId'] = "xiuouwerotueowr";
+        echo json_encode($response);
+        exit();
+    }
+
+    public function info() {
+        $id = $this->request->getParam('id');
+        $response = [];
+        $response['id'] = $id;
+        $response['walletAddress'] = "1";
+        $response['companyId'] = "1";
+        $response['productId'] = "1";
+        $response['variantId'] = "1";
+        $response['title'] = "Hat";
+        $response['name'] = "Nice Hat";
+        $response['quantity'] = "1";
+        $response['price'] = "1000.00";
+        $response['url'] = "hat.com";
+        $response['previewUrl'] = "abcde";
+        $response['numberOfCopies'] = "1";
+        $response['serviceFee'] = "1";
+        $response['unlockContent'] = "1";
+        $response['lockContent'] = "1";
+        $response['description'] = "no desc";
+        $response['storeId'] = "1";
+        $response['externalUrl'] = "external.com";
+        $response['itemType'] = "1";
+        $response['referenceId'] = "refer.com";
+        $response['collectionId'] = "1";
+        $response['referenceId'] = "1";
+        $response['itemRoute'] = "1";
+        $response['accountId'] = "1";
+        $response['creatorId'] = "1";
+        $response['originalItemId'] = "1";
+        $response['status'] = "1";
+        $response['liker'] = "1";
+        $response['expireTime'] = "2022-01-01";
+        $response['minimumBid'] = "1";
+        $response['highestBid'] = "1";
+        $response['listTime'] = "2022-01-01";
+        $response['tokenIds'] = "xasdfsdtwerwer";
+        $response['transactionId'] = "xiuouwerotueowr";
+        echo json_encode($response);
+        exit();
+    }
+
+    public function update() {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $data = $this->request->getData();
+        if (!$data['referenceId'] || !$data['numberChange']) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+        $response = $data;
+        $response['id'] = 1;
+        $response['walletAddress'] = "1";
+        $response['companyId'] = "1";
+        $response['productId'] = "1";
+        $response['variantId'] = "1";
+        $response['title'] = "Hat";
+        $response['name'] = "Nice Hat";
+        $response['quantity'] = "1";
+        $response['price'] = "1000.00";
+        $response['url'] = "hat.com";
+        $response['previewUrl'] = "abcde";
+        $response['numberOfCopies'] = $data['numberChange'];
+        $response['serviceFee'] = "1";
+        $response['unlockContent'] = "1";
+        $response['lockContent'] = "1";
+        $response['description'] = "no desc";
+        $response['storeId'] = "1";
+        $response['externalUrl'] = "external.com";
+        $response['itemType'] = "1";
+        // $response['referenceId'] = "refer.com";
+        $response['collectionId'] = "1";
+        $response['referenceId'] = "1";
+        $response['itemRoute'] = "1";
+        $response['accountId'] = "1";
+        $response['creatorId'] = "1";
+        $response['originalItemId'] = "1";
+        $response['status'] = "1";
+        $response['liker'] = "1";
+        $response['expireTime'] = "2022-01-01";
+        $response['minimumBid'] = "1";
+        $response['highestBid'] = "1";
+        $response['listTime'] = "2022-01-01";
+        $response['tokenIds'] = "xasdfsdtwerwer";
+        $response['transactionId'] = "xiuouwerotueowr";
+        echo json_encode($response);
+        exit();
+    }
+
+    public function buy() {
+        if (!$this->request->isPost()) {
+            $response = $this->response->withStatus(405);
+            return $response;
+        }
+        $data = $this->request->getData();
+        if (!$data['itemId']) {
+            $response = $this->response->withStatus(500);
+            return $response;
+        }
+        $response = $data;
+        $response['id'] = $data['itemId'];
+        $response['walletAddress'] = "1";
+        $response['companyId'] = "1";
+        $response['productId'] = "1";
+        $response['variantId'] = "1";
+        $response['title'] = "Hat";
+        $response['name'] = "Nice Hat";
+        $response['quantity'] = "1";
+        $response['price'] = "1000.00";
+        $response['url'] = "hat.com";
+        $response['previewUrl'] = "abcde";
+        $response['numberOfCopies'] = 100;
+        $response['serviceFee'] = "1";
+        $response['unlockContent'] = "1";
+        $response['lockContent'] = "1";
+        $response['description'] = "no desc";
+        $response['storeId'] = "1";
+        $response['externalUrl'] = "external.com";
+        $response['itemType'] = "1";
+        $response['referenceId'] = "refer.com";
+        $response['collectionId'] = "1";
+        $response['referenceId'] = "1";
+        $response['itemRoute'] = "1";
+        $response['accountId'] = "1";
+        $response['creatorId'] = "1";
+        $response['originalItemId'] = "1";
+        $response['status'] = "1";
+        $response['liker'] = "1";
+        $response['expireTime'] = "2022-01-01";
+        $response['minimumBid'] = "1";
+        $response['highestBid'] = "1";
+        $response['listTime'] = "2022-01-01";
+        $response['tokenIds'] = "xasdfsdtwerwer";
+        $response['transactionId'] = "xiuouwerotueowr";
+        echo json_encode($response);
+        exit();
+    }
+}
